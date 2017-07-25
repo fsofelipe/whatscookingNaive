02b55e2e9bcf3d33b86ae3827e6a2de3ddcfbd4b
fsofelipe <felipe.oliveira.tk@gmail.com>
2017-07-25 09:11:07 -0300

    estrutura dos tipos colinarios 2

--- main.h
+++ main.h
@@ -1,16 +1,12 @@
-//
-// Created by matheus on 23/07/17.
-//
-
-
 #ifndef WHATSCOOKINGNAIVE_MAIN_H
 #define WHATSCOOKINGNAIVE_MAIN_H
 
 typedef struct recipe {
-    int id;
-    char* cuisine;
-    char** ingredients;
-    int *ingredientsFreq;
+    int id; //cuisine id
+    char* cuisine; //cuisine name
+    int counter; //cuisine counter
+    char** ingredients; //list of ingredients that are part of the cuisine
+    int *ingredientsFreq; //list of the frequency for each ingredient on the cuisine
 } Recipe;
 
 #endif //WHATSCOOKINGNAIVE_MAIN_H
