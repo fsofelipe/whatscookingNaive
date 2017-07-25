aa58c023f7d58ea713d9db01838abe23b4c5b02c
matheusjkweber <matheus.jk.weber@gmail.com>
2017-07-25 15:27:12 -0300

    Changes manually

--- main.h
+++ main.h
@@ -1,17 +1,26 @@
 #ifndef WHATSCOOKINGNAIVE_MAIN_H
 #define WHATSCOOKINGNAIVE_MAIN_H
 
-typedef struct recipe {
-    char* ingredients; //ingredients that are part of the cuisine
-    int ingredientsFreq; //requency for each ingredient on the cuisine
-} recipe_t;
+typedef struct ingredient{
+    char * name;
+    long double frequency;
+} Ingredient;
 
-typedef struct cuisine{
-  int id; //cuisine id
-  char* name; //cuisine name
-  int counter; //cuisine counter; how many times the cuisine appears on the dataset
-  recipe_t *ingredient; //list of ingredients for each recipe
-  int totalIngredients;
-} cuisine_t;
+typedef struct recipe {
+    int id; //cuisine id
+    char* cuisine; //cuisine name
+    int counter; //cuisine counter
+    Ingredient * ingredients; //list of ingredients that are part of the cuisine
+    int number_ingredients;
+} Recipe;
+
+typedef struct list_of_recipes{
+    int number;
+    Recipe **list;
+} List_Of_Recipes;
+
+void print_recipe(Recipe *recipe, int withCuisine);
+
+void print_recipe_list(List_Of_Recipes *list, int withCuisine);
 
 #endif //WHATSCOOKINGNAIVE_MAIN_H
